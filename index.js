c8c9ecb7948b69a1567a50dbba6134e6dc363b9b
wangheyao <wangheyao@zealfi.com>
2021-04-25 11:25:09 +0800

    fix

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const Koa = require('koa')
 const Router = require('koa-router')
 const app = new Koa()
-const router = new Router()
 
 app.use(async (ctx)=>{
   ctx.body = '404'
 })
 
+const router = new Router()
 router.get('/', async (ctx) =>{
   ctx.body = ctx.request.url
 })
-
 router.get('/about', async (ctx) =>{
   ctx.body = 'page about'
 })
