2d6e6896da119aa9fbb0f646c9df83c5d59b9816
wangheyao <wangheyao@zealfi.com>
2021-04-25 14:31:03 +0800

    post请求

--- index.js
+++ index.js
@@ -2,19 +2,71 @@ const Koa = require('koa')
 const Router = require('koa-router')
 const app = new Koa()
 
-app.use(async (ctx, next)=>{
-  await next()
-  if(parseInt(ctx.status) === 404){  // 没有资源
-    ctx.response.redirect("/404")
+
+function parseQueryStr( queryStr ) {
+  let queryData = {}
+  let queryStrList = queryStr.split('&')
+  for (  let [ index, queryStr ] of queryStrList.entries()  ) {
+    let itemList = queryStr.split('=')
+    queryData[ itemList[0] ] = decodeURIComponent(itemList[1])
+  }
+  return queryData
+}
+
+function parsePostData( ctx ) {
+  return new Promise((resolve, reject) => {
+    try {
+      let postdata = "";
+      ctx.req.addListener('data', (data) => {
+        postdata += data
+      })
+      ctx.req.addListener("end",function(){
+        let parseData = parseQueryStr( postdata )
+        resolve( parseData )
+      })
+    } catch ( err ) {
+      reject(err)
+    }
+  })
+}
+
+app.use( async ( ctx ) => {
+  console.log(ctx.url)
+  console.log(ctx.method)
+
+  if ( ctx.url === '/' && ctx.method === 'GET' ) {
+    // 当GET请求时候返回表单页面
+    let html = `
+      <h1>koa2 request post demo</h1>
+      <form method="POST" action="/">
+        <p>userName</p>
+        <input name="userName" /><br/>
+        <p>nickName</p>
+        <input name="nickName" /><br/>
+        <p>email</p>
+        <input name="email" /><br/>
+        <button type="submit">submit</button>
+      </form>
+    `
+    ctx.body = html
+  } else if ( ctx.url === '/' && ctx.method === 'POST' ) {
+    // 当POST请求的时候，解析POST表单里的数据，并显示出来
+    let postData = await parsePostData( ctx )
+    console.log(postData)
+    ctx.body = postData
+  } else {
+    // 其他请求显示404
+    ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
   }
 })
 
+
 const router = new Router()
-router.get('/', async (ctx) =>{
-  ctx.body = ctx.request.url
-})
 router.get('/about', async (ctx) =>{
-  ctx.body = 'page about'
+  ctx.body = {
+    query: ctx.query,
+    queryString: ctx.querystring
+  }
 })
 app.use(router.routes())
 
