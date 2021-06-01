096936b9e508d8e2f17e4f9401faa273b1c0acbe
wangheyao <wangheyao@zealfi.com>
2021-06-01 15:59:59 +0800

    fix

---
+++ app/lib/enums.js
@@ -0,0 +1,26 @@
+function isThisType(val){
+  for(let key in this){
+      if(this[key] === val){
+          return true
+      }
+  }
+  return false
+}
+
+const AuthType = {
+  USER: 8,
+  ADMIN: 16,
+  SUPER_ADMIN: 32,
+  isThisType
+}
+
+const TokenType = {
+  ACCESS: 'access',
+  REFRESH: 'refresh'
+}
+
+
+module.exports = {
+  AuthType,
+  TokenType
+}
