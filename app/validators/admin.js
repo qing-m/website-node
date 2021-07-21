7460e176a79917540fee748331d2c8a6a5e5bee9
wangheyao <1874373481@qq.com>
2021-07-21 15:01:58 +0800

    更改验证

--- app/validators/admin.js
+++ app/validators/admin.js
@@ -2,12 +2,12 @@ const { LinValidator, Rule } = require('@core/lin-validator')
 
 const regExpPasswore = '^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]'
 const regExpNickName = '^[\u4e00-\u9fa5]{0,}$'
-const regExpLength = '/^(a-z|A-Z|0-9)*[^$%^&*;:,<>?()\""\']{1,1}$/'
+
 class CreateAuthNumberValidator extends LinValidator {
   constructor() {
     super()
     this.email = [
-      new Rule('isEmail', '请输入正确的Email')
+      new Rule('isEmail', '请输入正确的邮箱')
     ],
     this.password = [
       new Rule('isLength', '密码长度为6~32个字符', {
@@ -25,11 +25,11 @@ class CreateAuthNumberValidator extends LinValidator {
 class LoginValidator extends LinValidator {
   constructor() {
     super()
-    this.nickName = [
-      new Rule('matches', '账户不可为空', regExpLength)
+    this.email = [
+      new Rule('isEmail', '请输入正确的邮箱')
     ]
     this.password = [
-      new Rule('matches', '密码不可为空', regExpLength)
+      new Rule('matches', '密码不符合规范', regExpPasswore)
     ]
   }
 }
